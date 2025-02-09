f97057ca50f5468d69a35c42e630dccf16629181
fatmambarki <fatma.mbarki@esprit.tn>
2025-02-09 15:58:36 +0100

    eventsCRUD

--- src/Entity/Reservation.php
+++ src/Entity/Reservation.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\ReservationRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ReservationRepository::class)]
 class Reservation
@@ -13,33 +14,26 @@ class Reservation
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column]
-    private ?float $prixE = null;
-
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: "Le libellé ne doit pas être vide.")]
+    #[Assert\Length(max: 255, maxMessage: "Le libellé ne peut pas dépasser 255 caractères.")]
     private ?string $libelle = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: "L'état ne doit pas être vide.")]
     private ?string $etatE = null;
 
     #[ORM\ManyToOne]
     private ?Event $relation = null;
 
+    #[ORM\Column]
+    #[Assert\NotBlank(message: "Le nombre de places ne doit pas être vide.")]
+    #[Assert\GreaterThan(0, message: "Le nombre de places doit être supérieur à 0.")]
+    private ?int $nb_place = null;
+
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getPrixE(): ?float
-    {
-        return $this->prixE;
-    }
-
-    public function setPrixE(float $prixE): static
-    {
-        $this->prixE = $prixE;
-
-        return $this;
     }
 
     public function getLibelle(): ?string
@@ -50,7 +44,6 @@ class Reservation
     public function setLibelle(string $libelle): static
     {
         $this->libelle = $libelle;
-
         return $this;
     }
 
@@ -62,7 +55,6 @@ class Reservation
     public function setEtatE(string $etatE): static
     {
         $this->etatE = $etatE;
-
         return $this;
     }
 
@@ -74,7 +66,17 @@ class Reservation
     public function setRelation(?Event $relation): static
     {
         $this->relation = $relation;
+        return $this;
+    }
 
+    public function getNbPlace(): ?int
+    {
+        return $this->nb_place;
+    }
+
+    public function setNbPlace(?int $nb_place): static
+    {
+        $this->nb_place = $nb_place;
         return $this;
     }
 }
